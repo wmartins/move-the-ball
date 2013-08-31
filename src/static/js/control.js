6d1bd66e84b6349ae27a3d829d8fc1e00a62c56e
William Martins <wwwhmartins@gmail.com>
2013-08-31 01:38:24 -0300

    Add keyboard movement and changes onclick events to addEventListener

--- src/static/js/control.js
+++ src/static/js/control.js
@@ -1,4 +1,4 @@
-(function(io) {
+(function(window, io) {
 
     var ctrl = ctrl || {
         buttons: {
@@ -14,30 +14,67 @@
             n: n
         });
     },
-    socket = io.connect();
+    socket = io.connect(),
+    click_move = function(fn) {
+        var interval;
+        this.addEventListener('mousedown', function(event){
+            if(event.which === 1) { // left button
+                interval = setInterval(fn,0);
+            }
+        });
+        this.addEventListener('mouseup', function(event){
+            if(event.which === 1) { // left button
+                clearInterval(interval);
+            }
+        });
+    },
+    keyboard_move = function(key, fn) {
+        var interval,
+        released = true;
+        this.addEventListener('keydown', function(event){
+            if(released && event.which === key) {
+                interval = setInterval(fn,0);
+                released = false;
+            }
+        });
+        this.addEventListener('keyup', function(event){
+            if(event.which === key) {
+                clearInterval(interval);
+                released = true;
+            }
+        });
+    };
 
-    ctrl.buttons.top.onclick = (function() {
-        return function () {
-            move.call(socket, 'top', 100);
-        };
-    }());
+    click_move.call(ctrl.buttons.top, function(){
+        move.call(socket, 'top', 1);
+    });
 
-    ctrl.buttons.left.onclick = (function() {
-        return function () {
-            move.call(socket, 'left', 100);
-        };
-    }());
+    click_move.call(ctrl.buttons.right, function(){
+        move.call(socket, 'right', 1);
+    });
 
-    ctrl.buttons.right.onclick = (function() {
-        return function () {
-            move.call(socket, 'right', 100);
-        };
-    }());
+    click_move.call(ctrl.buttons.left, function(){
+        move.call(socket, 'left', 1);
+    });
 
-    ctrl.buttons.bottom.onclick = (function() {
-        return function () {
-            move.call(socket, 'bottom', 100);
-        };
-    }());
+    click_move.call(ctrl.buttons.bottom, function(){
+        move.call(socket, 'bottom', 1);
+    });
 
-}(io));
+    keyboard_move.call(window, 38, function(){ // 38 - up arrow
+        move.call(socket, 'top', 1);
+    });
+
+    keyboard_move.call(window, 37, function(){ // 37 - left arrow
+        move.call(socket, 'left', 1);
+    });
+
+    keyboard_move.call(window, 39, function(){ // 39 - right arrow
+        move.call(socket, 'right', 1);
+    });
+
+    keyboard_move.call(window, 40, function(){ // 40 - down arrow
+        move.call(socket, 'bottom', 1);
+    });
+
+}(window, io));
